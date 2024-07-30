3d13aac15c8bd8ebdb3ee1dcbc090178aa9d2f5e
Ignacio Grane <graneignacio@gmail.com>
2024-07-30 12:44:54 -0300

    Ejercicio 01 - Finish

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,17 +1,14 @@
-const Tarjeta = () => {
+const Tarjeta = (props) => {
+  const { nombre, profesion, foto } = props;
   return (
     <>
-      <div className="card" style={{width: '18rem'}}>
-        <img className="card-img-top" />
+      <h2>Ejercicio 01</h2>
+      <hr />
+      <div className="card ms-3" style={{ width: "18rem" }}>
+        <img className="card-img-top" src={foto} />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <h5 className="card-title">{nombre}</h5>
+          <p className="card-text">{profesion}</p>
         </div>
       </div>
     </>
